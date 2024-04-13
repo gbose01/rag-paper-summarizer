477e1bd398669a31be1cb0ad35781ee9dd550775
Gaurab Bose <89564216+gbose01@users.noreply.github.com>
2024-04-12 22:26:59 -0700

    commit all code

---
+++ htmlTemplates.py
@@ -0,0 +1,52 @@
+
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; 
+    border-radius: 0.5rem; 
+    margin-bottom: 1rem; 
+    display: flex;
+    align-items: center; /* Center align items vertically */
+}
+.chat-message.user {
+    background-color: #2b313e;
+}
+.chat-message.bot {
+    background-color: #475063;
+}
+.chat-message .avatar {
+    width: 20%;
+    display: flex; /* To center align content */
+    justify-content: center; /* To center align content */
+}
+.chat-message .avatar img {
+    max-width: 78px;
+    max-height: 78px;
+    border-radius: 50%;
+    object-fit: cover;
+}
+.chat-message .message {
+    width: 80%;
+    padding: 0 1.5rem;
+    color: #fff;
+}
+</style>
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="assets/brain.png">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="assets/man.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
